ab34c9c9482dfb0e76268b560499801256d38250
Unknown <g.delanoe@hotmail.fr>
2017-07-16 15:50:06 +0200

    Add async call
    translation of colorizeData from PHP to JS
    add jquery async call, creation of poller.php

--- dialog.php
+++ dialog.php
@@ -1,26 +1,21 @@
-<?php 
-function measureDisk($disk){
-	return round(100 - ((disk_free_space($disk) / 1024 / 1024 / 1024) / (disk_total_space($disk) / 1024 / 1024 / 1024) * 100), 2); 
-}
 
-function colorizeData($data){ 
-	if ($data < 50){
-		return "<b style='color:green;'>".$data."%</b>";
-	}
-	elseif($data < 75){
-		return "<b style='color:orange;'>".$data."%</b>";
-	}
-	elseif($data >= 75){
-		return "<b style='color:red;'>".$data."%</b>";
-	}
-}
-
+<?php
 $hostname = exec("hostname");
-$cpuUsage = exec('top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk \'{print 100 - $1}\'');
-$memoryUsage = round(shell_exec("free | grep Mem | awk '{print $3/$2 * 100.0}'"), 2);
-$diskUsage = measureDisk("/");
 $uptime = substr(exec("uptime -p"), 3);
 ?>
+
+<script>
+var killed = false;
+
+$(document).ready(function() {
+	$("#close-handle").attr("onclick","codiad.modal.unload(); killed = true; return false;");
+	killed = false;
+
+	poll();
+});
+
+
+</script>
 
 <h2 style="font-size:20px;">System Information</h2>
 <br>
@@ -28,33 +23,35 @@ $uptime = substr(exec("uptime -p"), 3);
 <p>Hostname: <strong><?php echo $hostname; ?></strong></p>
 <br>
 
+
+
 <table width="600px">
 	<tr>
 		<td>CPU Usage:</td>
-		<td><?php echo colorizeData($cpuUsage); ?></td>
+		<td id="cpuUsage"></td>
 		<td>
 			<span style="border:1px solid gray;border-radius:3px;width:150px;display:block;">
-				<span style="background-color:white;border-radius:3px;width:<?php echo $cpuUsage; ?>%;display:block;">&nbsp;</span>
+				<span style="background-color:white;border-radius:3px;display:block;" id="cpuBar">&nbsp;</span>
 			</span>
 		</td>
 	</tr>
 
 	<tr>
 		<td>Memory Usage:</td>
-		<td><?php echo colorizeData($memoryUsage); ?></td>
+		<td id="memoryUsage"></td>
 		<td>
 			<span style="border:1px solid gray;border-radius:3px;width:150px;display:block;">
-				<span style="background-color:white;border-radius:3px;width:<?php echo $memoryUsage; ?>%;display:block;">&nbsp;</span>
+				<span style="background-color:white;border-radius:3px;display:block;" id="memoryBar">&nbsp;</span>
 			</span>
 		</td>
 	</tr>
 
-	<tr>
-		<td>Used Disk Space:</td>
-		<td><?php echo colorizeData($diskUsage); ?></td>
+	<tr
+>		<td>Used Disk Space:</td>
+		<td id="diskUsage"></td>
 		<td>
 			<span style="border:1px solid gray;border-radius:3px;width:150px;display:block;">
-				<span style="background-color:white;border-radius:3px;width:<?php echo $diskUsage; ?>%;display:block;">&nbsp;</span>
+				<span style="background-color:white;border-radius:3px;display:block;" id="diskBar">&nbsp;</span>
 			</span>
 		</td>
 	</tr>
